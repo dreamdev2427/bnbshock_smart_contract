105b39761f35b5623abc5dac9d66bb8de20e2f23
CrystalBlockDev <crytal.blockdev@gmail.com>
2022-09-23 01:14:12 +0900

    update for referral

--- PairVetting.sol
+++ PairVetting.sol
@@ -9,7 +9,8 @@ contract PairVetting is Ownable
 {    
 	using SafeMath for uint256;
 
-	address public gameManager = 0x3745CCE8D73fF51376B1B3E5639f290EE5147187;
+	address public gameManager = 0x4B129178704A94b112D7dF860C91986Fe11Ad23F;
+	uint public claimDuration = 24 * 3600 * 7;
 
 	struct winner
 	{
@@ -17,42 +18,74 @@ contract PairVetting is Ownable
 		uint amount;
 		string idOnDB;
 	}
+	mapping( address => uint256 ) refAwardAmount;
+	mapping( address => uint256 ) countOfRerrals;
+  mapping( address => uint256 ) claimedTime;
 
 	event StartOfVetting(address wallet, string pairId ,uint pairPrice, uint amount, uint vettingPeriod, bool upOrDown);
 	event EndOfVetting(winner[] winners);
 	event Maintenance(address owner, uint tokenBalance, uint nativeBalance);
 	event ChangedGameManager(address owner, address newManager);
+	event ChangedClaimDuration(address owner, uint newDuration);
+	event ClaimedAward(address wallet, uint awardAmount);
 	
+	function changeClaimDuration(uint newDuration) external {
+		require(msg.sender == gameManager || msg.sender == owner());
+		claimDuration = newDuration;
+		emit ChangedClaimDuration(owner(), claimDuration);
+	}
+
 	function changeGameManager(address newAddr) external {
-	    require(msg.sender == gameManager);
-	    gameManager = newAddr;
+		require(msg.sender == gameManager || msg.sender == owner());
+		gameManager = newAddr;
 		emit ChangedGameManager(owner(), gameManager);
 	}
 
-	function enterVetting(string memory pairId, uint amount, uint pairPrice, uint vettingPeriod, bool upOrDown) external payable    
-	{
-		emit StartOfVetting(msg.sender, pairId, amount, pairPrice, vettingPeriod, upOrDown);
+	function getClaimableInformation(address user) public view returns(uint, uint, uint) {
+		return (refAwardAmount[user], countOfRerrals[user], claimedTime[user]);
+	}
+
+	function claimReferralAwards(address user) external {
+		require( refAwardAmount[user] > 0, "103");
+    require( claimedTime[user] + claimDuration > block.timestamp, "102" );
+		require( address(this).balance > refAwardAmount[user], "101");
+
+ 		payable(user).transfer(refAwardAmount[user]);
+		emit ClaimedAward(user, refAwardAmount[user]);
+
+		refAwardAmount[user] = 0;
+		countOfRerrals[user] = 0;
+		claimedTime[user] = block.timestamp;
+	}
+
+	function enterVetting(string memory pairId, uint pairPrice, uint vettingPeriod, bool upOrDown, address ref) external payable    
+	{		
+		uint amount = msg.value;
+		uint awardAmount = amount.mul(2).div(100);
+		refAwardAmount[ref] += awardAmount;
+		countOfRerrals[ref] += 1;		
+		emit StartOfVetting(msg.sender, pairId, amount - awardAmount, pairPrice, vettingPeriod, upOrDown);
 	}
 
 	function endVetting(winner[] memory winners) external
 	{
-	    require(msg.sender == gameManager);
-        for(uint idx = 0; idx<winners.length; idx++)
-        {
-            uint256 nativeBal = address(this).balance;
-            require(nativeBal > winners[idx].amount, "Not enough funds.");
-            address payable mine = payable(winners[idx].wallet);
-            mine.transfer(winners[idx].amount);    
-        }
+		require(msg.sender == gameManager);
+		for(uint idx = 0; idx<winners.length; idx++)
+		{
+				uint256 nativeBal = address(this).balance;
+				require(nativeBal > winners[idx].amount, "101");
+				address payable mine = payable(winners[idx].wallet);
+				mine.transfer(winners[idx].amount);    
+		}
 		emit EndOfVetting(winners); 
 	}
 
 	function withdraw(address _addr) external onlyOwner
 	{
-	    require(msg.sender == owner());
+	  require(msg.sender == owner());
 		uint256 balance = IERC20(_addr).balanceOf(address(this));
 		if(balance > 0) {
-            IERC20(_addr).transfer(msg.sender, balance);
+      IERC20(_addr).transfer(msg.sender, balance);
 		}
 		uint256 nativeBal = address(this).balance;
 		if(nativeBal> 0) {
